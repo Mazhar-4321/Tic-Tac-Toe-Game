edfb0857a30aaeb943aa38a71b6819b33a2813af
Mazhar-4321 <syedmazharali742@gmail.com>
2022-12-05 16:09:08 +0530

    ADDED: Code to take Input from User and Determine Player and Computer Letter

--- src/com/company/TicTacToeGame.java
+++ src/com/company/TicTacToeGame.java
@@ -1,9 +1,33 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class TicTacToeGame {
+    private final Scanner scanner = new Scanner(System.in);
     private char[] board;
+    private char playerLetter;
+    private char computerLetter;
 
     public TicTacToeGame() {
         board = new char[10];
+    }
+
+    public void allowPlayerToGiveInput() {
+        System.out.println("Please Input a Character X or O");
+        while (true) {
+            String inputString = scanner.next();
+            if (inputString.length() > 1) {
+                System.out.println("Please Enter Character X or O");
+                continue;
+            }
+            if (inputString.equalsIgnoreCase("x") || inputString.equalsIgnoreCase("o")) {
+                playerLetter = inputString.charAt(0);
+                computerLetter = inputString.charAt(0) == 'o' ? 'x' : 'o';
+                break;
+            }
+            System.out.println("Please Enter Character X or O");
+        }
+
+
     }
 }
